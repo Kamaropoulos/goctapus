c6125c3f467aaf79e11b68b0728dfe6a192d9e93
Kamaropoulos <kamaropoulos@outlook.com>
2018-04-18 06:54:43 +0300

    Added port number check on server configuration

--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net"
 	"os"
+	"strconv"
 
 	"github.com/Kamaropoulos/go-echo-vue-mysql/handlers"
 
@@ -11,61 +13,102 @@ import (
 	"github.com/labstack/echo"
 )
 
+func isValidPort(str string) (bool, string) {
+	if i, err := strconv.Atoi(str); err == nil {
+		if (1 <= i) && (i <= 65535) {
+			return true, ""
+		} else {
+			return false, "A port number should be between 1 and 65535"
+		}
+	}
+	return false, "Not a valid port number"
+}
+
+func isUsablePort(str string) (bool, string) {
+	if res, err := isValidPort(str); !res {
+		return res, err
+	} else {
+		ln, err1 := net.Listen("tcp", ":"+str)
+
+		if err1 != nil {
+			return false, err1.Error()
+		}
+
+		_ = ln.Close()
+
+		return true, ""
+	}
+}
+
 func getArgs(args []string) (string, string, string, string, string) {
-	var appPort string
-	var dbHost string
-	var dbPort string
-	var dbUser string
-	var dbPass string
+
+	// Set default configuration values
+	appPort := "8000"
+	dbHost := "localhost"
+	dbPort := "3306"
+	dbUser := "root"
+	dbPass := ""
 
 	switch argsCount := len(args); argsCount {
-	case 0:
-		// No arguments were passed
-		// Using default configuration values
-		appPort = "8000"
-		dbHost = "localhost"
-		dbPort = "3306"
-		dbUser = "root"
-		dbPass = ""
 	case 1:
 		// There was 1 argument passed, use it as
 		// the port for the app to listen to
-		appPort = args[0]
-		dbHost = "localhost"
-		dbPort = "3306"
-		dbUser = "root"
-		dbPass = ""
+		if res, err := isUsablePort(args[0]); res {
+			appPort = args[0]
+		} else {
+			panic("\"" + args[0] + "\" can't be used as a port number: " + err)
+		}
+
 	case 2:
 		// There were 2 arguments passed
 		// Use them as DB username and password
-		appPort = "8000"
-		dbHost = "localhost"
-		dbPort = "3306"
 		dbUser = args[0]
 		dbPass = args[1]
+
 	case 3:
 		// There were 3 arguments passed
 		// Use them as App Port, DB Username and Password
-		appPort = args[0]
-		dbHost = "localhost"
-		dbPort = "3306"
+		if res, err := isUsablePort(args[0]); res {
+			appPort = args[0]
+		} else {
+			panic("\"" + args[0] + "\" can't be used as a port number: " + err)
+		}
+
 		dbUser = args[1]
 		dbPass = args[2]
+
 	case 4:
 		// There were 4 arguments passed
 		// Use them as App Port, DB Username, Password and Hostname/IP
-		appPort = args[0]
+		if res, err := isUsablePort(args[0]); res {
+			appPort = args[0]
+		} else {
+			panic("\"" + args[0] + "\" can't be used as a port number: " + err)
+		}
+
 		dbHost = args[3]
-		dbPort = "3306"
 		dbUser = args[1]
 		dbPass = args[2]
+
 	case 5:
 		// There were 5 arguments passed covering all configuration parameters
 		// Use them as App Port, DB Username, Password, Hostname/IP and Port
-		appPort = args[0]
+		if res, err := isUsablePort(args[0]); res {
+			appPort = args[0]
+		} else {
+			panic("\"" + args[0] + "\" can't be used as a port number: " + err)
+		}
+
 		dbHost = args[3]
 		dbPort = args[4]
 		dbUser = args[1]
+
+		if res, err := isValidPort(args[2]); res {
+			appPort = args[2]
+		} else {
+			panic("\"" + args[2] + "\" can't be used as a port number: " + err)
+		}
+
 		dbPass = args[2]
 	}
 
@@ -77,9 +120,9 @@ func main() {
 
 	fmt.Println(appPort, dbHost, dbPort, dbUser, dbPass)
 
-	db := initDB(dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/?charset=utf8")
-
-	migrate(db)
+	//db := initDB(dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/?charset=utf8")
+	var db *sql.DB
+	//migrate(db)
 
 	e := echo.New()
 
