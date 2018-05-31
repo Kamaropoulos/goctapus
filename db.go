c228bd78b12160e65d2be396779fa417df54327e
Kamaropoulos <kamaropoulos@outlook.com>
2018-05-31 18:38:24 +0300

    Added extra debug logging

--- db.go
+++ db.go
@@ -6,38 +6,62 @@ import (
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
+
+	Log "github.com/sirupsen/logrus"
 )
 
 // InitDB initializes the connection to the Database
-func InitDB(dbString string) *sql.DB {
+func InitDB(dbString string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dbString)
 
 	// Here we check for any db errors then exit
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// If we don't get any errors but somehow still don't get a db connection
 	// we exit as well
 	if db == nil {
-		panic("db nil")
+		return nil, nil
 	}
-	return db
+
+	// Ping to check if the server is up
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err;
+	}
+
+	return db, nil
 }
 
 func ConnectDB(db_name string) {
 
+	Log.Debug("Connecting to Database server on " + Config.dbHost + ":" + Config.dbPort + "...")
+
 	// Connects to a database and stores the connection to an object in the Databases Map
-	Databases[db_name] = InitDB(Config.dbUser + ":" + Config.dbPass + "@tcp(" + Config.dbHost + ":" + Config.dbPort + ")/?charset=utf8")
+	db, err := InitDB(Config.dbUser + ":" + Config.dbPass + "@tcp(" + Config.dbHost + ":" + Config.dbPort + ")/?charset=utf8")
+
+	if (err != nil) || (db == nil) {
+		Log.Fatal("Could not connect to the Database server: " + err.Error())
+	}
+
+	Databases[db_name] = db
+
+	Log.Debug("Connected to Database server.")
+
+	Log.Debug("Creating DB \"" + db_name + "\" if it doesn't already exist...")
 
 	// Run the Database creation and USE queries
 	sql := `CREATE DATABASE IF NOT EXISTS ` + db_name + `;
 			USE ` + db_name + `;`
-	executeSQLString(Databases[db_name], sql)
+	err = executeSQLString(Databases[db_name], sql)
+	if err != nil {
+		Log.Fatal("An error occured while creating DB \"" + db_name + "\"")
+	}
 
 }
 
-func executeSQLString(db *sql.DB, script string) {
+func executeSQLString(db *sql.DB, script string) error {
 	// split it into seperate queries
 	queries := strings.Split(script, ";")
 
@@ -46,22 +70,32 @@ func executeSQLString(db *sql.DB, script string) {
 	for _, query := range queries[0 : len(queries)-1] {
 		_, err := db.Exec(query)
 		if err != nil {
-			panic(err)
+			return err;
 		}
 	}
+	return nil;
 }
 
-func executeSQLFile(db *sql.DB, pathtofile string) {
+func executeSQLFile(db *sql.DB, pathtofile string) error {
 	file, err := ioutil.ReadFile(pathtofile)
 
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	executeSQLString(db, string(file))
+	return executeSQLString(db, string(file))
 }
 
 // Migrate xecutes an SQL File against a specific DB
 func Migrate(db *sql.DB, filename string) {
-	executeSQLFile(db, filename)
+
+	Log.Debug("Exexuting SQL file \"" + filename + "\"...")
+
+	err := executeSQLFile(db, filename)
+
+	if err != nil {
+		Log.Fatal("Executing " + filename + " failed: " + err.Error())
+	}
+
+	Log.Debug("SQL file \"" + filename + "\" was executed succesfully.")
 }
